5e178d216d809054da61cd5d4cf3ccd3f005bcdd
nkped <nikrpedev@gmail.com>
2022-04-25 22:19:50 +0200

    Before I tried to update the React v.18 render method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import Card from "react-bootstrap/Card";
 
 function App() {
   return (
-    <div>
-      <h1>Portfolio header</h1>
+    <div className="App">
+      <header className="App-header">
+        <Breadcrumb>
+          <Breadcrumb.Item>Test1</Breadcrumb.Item>
+          <Breadcrumb.Item>Test2</Breadcrumb.Item>
+          <Breadcrumb.Item>Test3</Breadcrumb.Item>
+        </Breadcrumb>
+        <Button>First button</Button>
+        <Alert variant="succes">This is a button</Alert>
+      </header>
     </div>
   );
 }
