f535b63494f2b2135ffb4073de9c6f0004278a80
nkped <nikrpedev@gmail.com>
2022-04-26 17:29:56 +0200

    New createRoot throws no errors, even in StrictMode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+//React
+import { Link } from "react-router-dom";
+
+//Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
@@ -8,15 +12,7 @@ import Card from "react-bootstrap/Card";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Breadcrumb>
-          <Breadcrumb.Item>Test1</Breadcrumb.Item>
-          <Breadcrumb.Item>Test2</Breadcrumb.Item>
-          <Breadcrumb.Item>Test3</Breadcrumb.Item>
-        </Breadcrumb>
-        <Button>First button</Button>
-        <Alert variant="succes">This is a button</Alert>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { StrictMode } from "react";
+import * as ReactDOMClient from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import "./index.css";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+const root = ReactDOMClient.createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
